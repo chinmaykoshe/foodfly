9d628326f1aa81d37982521a001b143d77f3e98a
chinmaykoshe <141853866+chinmaykoshe@users.noreply.github.com>
2025-10-18 22:49:00 +0530

    cors err

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const app = require("../server");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000","https://vercel.com/chinmaykoshes-projects/foodfly"],
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-user-id"]
+}));
 
 // ✅ Root route for quick status check
 app.get("/", (req, res) => {
